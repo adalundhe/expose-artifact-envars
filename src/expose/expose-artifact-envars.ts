5b16c0b03a9d493446acc1fafe91a1f3fcc5d96d
Ada Lundhe <ada@datavant.com>
2025-05-20 14:08:39 -0500

    AL: fix

--- src/expose/expose-artifact-envars.ts
+++ src/expose/expose-artifact-envars.ts
@@ -1,6 +1,37 @@
 import * as core from '@actions/core'
 import {getInputs} from './input-helper'
 import fs from 'fs'
+import path from 'path'
+
+type Envars = {
+  actions_results_url: string
+  actions_runtime_token: string
+}
+
+const pathExistsAsync = async (path: string): Promise<boolean> => {
+  const promise = new Promise<boolean>((resolve, reject) => {
+    try {
+      resolve(fs.existsSync(path))
+    } catch (err) {
+      reject(err)
+    }
+  })
+
+  return await promise
+}
+
+const createOutputFile = async (filepath: string, envars: Envars) => {
+  const outputFile = path.basename(filepath)
+  const directory = filepath.replace(outputFile, '')
+
+  const directoryExists = await pathExistsAsync(directory)
+
+  if (!directoryExists) {
+    await fs.promises.mkdir(directory, {recursive: true})
+  }
+
+  await fs.promises.writeFile(filepath, JSON.stringify(envars))
+}
 
 export async function run(): Promise<void> {
   const inputs = getInputs()
@@ -15,10 +46,12 @@ export async function run(): Promise<void> {
   const runtimeToken = process.env['ACTIONS_RUNTIME_TOKEN']
   core.info(`Using the Artifact Results URL ${baseUrl}`)
 
-  const actionsEnvars = {
-    actions_results_url: new URL(baseUrl).origin,
-    actions_runtime_token: runtimeToken
+  if (runtimeToken === undefined) {
+    throw new Error('ACTIONS_RUNTIME_TOKEN is empty')
   }
 
-  await fs.promises.writeFile(inputs.outputPath, JSON.stringify(actionsEnvars))
+  await createOutputFile(inputs.outputPath, {
+    actions_results_url: new URL(baseUrl).origin,
+    actions_runtime_token: runtimeToken
+  })
 }
